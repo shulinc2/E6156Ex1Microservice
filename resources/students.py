a212aa0b7ee1863053818cad576eece3862c2e40
shulinc1 <shulinc2@uci.edu>
2023-09-18 23:59:15 -0400

    Initial commit

---
+++ resources/students.py
@@ -0,0 +1,20 @@
+import json
+
+
+class StudentsResource:
+    #
+    # This code is just to get us started.
+    # It is also pretty sloppy code.
+    #
+
+    students_file = \
+        "/Users/shulinchen/PycharmProjects/E6156Ex1Microservice/resources/students.json"
+
+    def __init__(self):
+        self.students = None
+
+        with open(self.students_file, "r") as j_file:
+            self.students = json.load(j_file)
+
+    def get_students(self):
+        return self.students
